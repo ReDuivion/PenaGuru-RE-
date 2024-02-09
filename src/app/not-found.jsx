515b4d169b105035e0b7606c108a82f2812923e3
unknown <Nabalzam>
2024-02-09 21:45:56 +0700

    Not Found - Selesai

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,10 +1,18 @@
+import Image from "next/image"
+import pagenotfound from "../../public/pagenotfound.png"
 export default function notFound() {
   return (
     <>
-      <main className="h-screen flex items-center justify-center">
-        <div className="text-2xl font-bold">404 Not Found</div>
-        <img src="https://dummyimage.com/600x400/000/fff"/>
+    <main className="flex justify-center">
+      <main className="h-screen items-center">
+        <div className="text-2xl font-bold"></div>
+        <Image src={pagenotfound} />
+        <div className="">
+        <h1 className="flex justify-center text-3xl font-bold text-sky-500 mt-3">Page Not Found!</h1>
+        <h1 className="mt-3">Page yang anda cari mungkin telah dipindahkan, dihapus, atau mungkin tidak pernah ada!</h1>
+        </div>
       </main>
+    </main>
     </>
   );
 }
