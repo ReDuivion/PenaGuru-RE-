c49f34fc833092d987a959abd70e3128ed4f802c
Ririena <nerllysurina@gmail.com>
2024-03-01 14:22:29 +0700

    Add Loader And Test For Functionality

---
+++ src/app/utils/Skeletons.jsx
@@ -0,0 +1,19 @@
+
+"use client"
+import React from 'react'
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Skeleton,
+  } from "@nextui-org/react";
+
+export default function Skeletons() {
+  return (
+    <Skeleton className='bg-sky-600'>
+
+    <h1>tes</h1>
+    </Skeleton>
+  )
+}
